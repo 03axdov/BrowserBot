b125652cfb15934b64f2ac3b38674ccb9667b88a
Axel Dovskog <axel.dovskog@outlook.com>
2022-03-16 22:47:05 +0100

    Added PAGE_URL

--- page.py
+++ page.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-BOOKING_NUMBER = "123131"    # Bokningsnummer
-EMAIL = "a@g.com"  # Email
+BOOKING_NUMBER = "123131"
+EMAIL = "a@g.com"
+PAGE_URL = "https://bokapass.nemoq.se/Booking/Booking/Index/stockholm"
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://bokapass.nemoq.se/Booking/Booking/Index/stockholm')
+driver.get(PAGE_URL)
 
 driver.find_element(By.ID, "BookingNumber").send_keys(BOOKING_NUMBER)
 driver.find_element(By.ID, "ContactInfo").send_keys(EMAIL)
