2fd84d083453ac0ddcba895963e339e3cc1b897d
Axel Dovskog <axel.dovskog@outlook.com>
2022-03-17 19:40:53 +0100

    Finished project

--- page.py
+++ page.py
@@ -20,28 +20,37 @@ current = dates.index("1 jul")
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(PAGE_URL)
 
-driver.find_element(By.ID, "BookingNumber").send_keys(BOOKING_NUMBER)
-driver.find_element(By.ID, "ContactInfo").send_keys(EMAIL)
+def login():
+    driver.find_element(By.ID, "BookingNumber").send_keys(BOOKING_NUMBER)
+    driver.find_element(By.ID, "ContactInfo").send_keys(EMAIL)
 
-driver.find_element(By.NAME, "NextButtonID6").click()
-driver.find_element(By.NAME, "NextButtonID26").click()
-driver.find_element(By.NAME, "TimeSearchFirstAvailableButton").click()
+    driver.find_element(By.NAME, "NextButtonID6").click()
+    driver.find_element(By.NAME, "NextButtonID26").click()
+    driver.find_element(By.NAME, "TimeSearchFirstAvailableButton").click()
+
+login()
 
 while True:
-    row = driver.find_elements(by=By.XPATH, value='//*[@id="Main"]/form[2]/div[2]/table/thead/tr/th')
-    date = row[1].text
-    if date[5:] not in dates:
-        driver.refresh()
-        continue
-    elif dates.index(date[5:]) < current:
-        elements = driver.find_elements(by=By.XPATH, value='//*[@id="Main"]/form[2]/div[2]/table/tbody/tr/td/div/div')
-        for day in elements[1:]:
-            if day.text != "Bokad":
-                day.click()
-                break
+    try:
+        row = driver.find_elements(by=By.XPATH, value='//*[@id="Main"]/form[2]/div[2]/table/thead/tr/th')
+        date = row[1].text
+        if date[5:] not in dates:
+            driver.refresh()
+            continue
+        elif dates.index(date[5:]) < current:
+            elements = driver.find_elements(by=By.XPATH, value='//*[@id="Main"]/form[2]/div[2]/table/tbody/tr/td/div/div')
+            for day in elements[1:]:
+                if day.text != "Bokad":
+                    day.click()
+                    break
 
-        driver.find_element(By.NAME, "Next").click()
-        break
+            current = date[5:]
+            driver.find_element(By.NAME, "Next").click()
+
+    except:
+        login()
+
+
 
 time.sleep(100000)
 
